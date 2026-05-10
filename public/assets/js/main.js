a60cceadc95d9780a380eef55c37134f1f6415bf
dzikrirahman175 <dzikrirahman175@gmail.com>
2026-05-10 14:17:38 +0700

    fix galeri

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'URL_SUPABASE';
-const supabaseKey = 'ANON_KEY';
+const supabaseUrl = 'https://vicfkqqaiawphrxnvcpf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpY2ZrcXFhaWF3cGhyeG52Y3BmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MzYwMjUsImV4cCI6MjA5MzUxMjAyNX0.9aM3wA5m8CN_MOiwv0zaT6csdnKdh7zRoE8vvRk6Y7o';
 
 const supabase =
     window.supabase.createClient(
